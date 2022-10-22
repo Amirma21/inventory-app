4af5c339cc95578097ab7a6e95128670f55dece6
Amir <amirmaleki21physics@gmail.com>
2022-10-22 16:38:22 +0330

    categoryList state lifted up to App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,48 @@
 <template>
   <v-app>
     <v-main>
-      <HelloWorld/>
+      <v-app-bar>
+        <div class="w-100 d-flex justify-center">
+          <h4>Hello!</h4>
+        </div>
+      </v-app-bar>
+      <v-row class="pa-8">
+        <v-col cols="6">
+          <add-category v-model="newCategory" @add-category="addToCategoryList"/>
+          <add-product/>
+        </v-col>
+      </v-row>
     </v-main>
   </v-app>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
+<script setup lang="ts">
+import AddCategory from "@/views/AddCategory.vue";
+import {ref} from "vue";
+import {addToLocalStorage, getFromLocalStorage} from "@/localStorge";
+import {Category} from "@/types";
+import AddProduct from "@/views/AddProduct.vue";
 
-export default defineComponent({
-  name: 'App',
+/// this method return array of objects !
+const getCategories = getFromLocalStorage('category') ?? []
 
-  components: {
-    HelloWorld,
-  },
+const categoryList = ref<Category[]>(getCategories)
 
-  data () {
-    return {
-      //
-    }
-  },
+const newCategory = ref({
+  title: '',
+  description: ''
 })
+
+
+function addToCategoryList() {
+  categoryList.value?.push({
+    title: newCategory.value.title,
+    description: newCategory.value.description,
+    createDate: Date.now()
+  })
+  addToLocalStorage('category', categoryList.value)
+  newCategory.value.title = newCategory.value.description = ''
+}
+
+
 </script>
