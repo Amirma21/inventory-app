2cf680978e6561c142e2c613e9ce9f4e9621802a
Amir <amirmaleki21physics@gmail.com>
2022-10-22 16:30:02 +0330

    types file

---
+++ src/types/index.ts
@@ -0,0 +1,11 @@
+export interface Category {
+    title: string
+    createDate: number
+    description: string
+}
+
+export interface Product {
+    title: string
+    quantity: number
+    categoryTitle:string
+}
