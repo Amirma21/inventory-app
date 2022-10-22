ffbef4f576178184bc8fe4c8b14060ebef100150
Amir <amirmaleki21physics@gmail.com>
2022-10-22 16:35:24 +0330

    add products and add category froms

--- src/views/AddCategory.vue
+++ src/views/AddCategory.vue
@@ -1,11 +1,56 @@
 <template>
-
+  <div v-if="isShowAddCategoryForm">
+    <h3>Add category</h3>
+    <v-form class="border rounded-lg pa-8 bg-primary">
+      <v-row>
+        <v-col cols="12">
+          <label class="font-weight-bold">title</label>
+          <a-input
+              v-model="value.title"
+              type="text"
+          />
+          <label class="font-weight-bold">description</label>
+          <v-textarea
+              v-model="value.description"
+              type="text"
+          />
+        </v-col>
+        <a-button @click="isShowAddCategoryForm = false" placeholder="cancel" variant="outlined"/>
+        <a-button @click="emits('addCategory')" placeholder="Add category"/>
+      </v-row>
+    </v-form>
+  </div>
+  <h4 @click="isShowAddCategoryForm = true" v-else>add new category ?</h4>
 </template>
 
-<script>
-export default {
-name: "AddCategory"
-}
+<script setup lang="ts">
+
+import AInput from "@/components/AInput.vue";
+import {ref, defineEmits, defineProps, PropType, computed} from "vue";
+import AButton from "@/components/AButton.vue";
+import {Category} from "@/types";
+
+
+const isShowAddCategoryForm = ref(false)
+
+const props = defineProps({
+  modelValue: {required: true, type: Object as PropType<Category>}
+})
+
+const emits = defineEmits([
+  'addCategory', 'update:modelValue'
+])
+
+const value = computed<Category>({
+  get: () => {
+    return props.modelValue
+  },
+  set: (newValue) => {
+    emits('update:modelValue', newValue)
+  }
+})
+
+
 </script>
 
 <style scoped>
--- src/views/AddProduct.vue
+++ src/views/AddProduct.vue
@@ -1,11 +1,38 @@
 <template>
-
+  <v-form class="border rounded-lg pa-8 bg-primary my-4">
+    <v-row justify="center">
+      <v-col cols="12">
+        <label class="font-weight-bold">title</label>
+        <a-input
+            v-model="newProduct.categoryTitle"
+        />
+        <label>quantity</label>
+        <a-input
+            v-model="newProduct.quantity"
+            type="number"
+        />
+        <label class="font-weight-bold">category</label>
+        <v-select :items="categoryTitleList"/>
+      </v-col>
+    </v-row>
+  </v-form>
 </template>
 
-<script>
-export default {
-name: "AddProduct"
-}
+<script setup lang="ts">
+
+import AInput from "@/components/AInput.vue";
+import {onMounted, ref} from "vue";
+import {Product} from "@/types";
+
+const newProduct = ref<Product>({categoryTitle: '', quantity: 0, title: ""})
+let categoryTitleList = []
+
+const categoryList = JSON.parse(localStorage.getItem('category')!)
+
+
+onMounted(() => categoryList.map((item: any) => categoryTitleList.push(item.title)))
+
+
 </script>
 
 <style scoped>
