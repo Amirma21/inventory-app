8cb6ddfd646e02508e044544b1882484ea8b4473
Amir <amirmaleki21physics@gmail.com>
2022-10-22 16:34:32 +0330

    usful components has been implemented

--- src/components/AButton.vue
+++ src/components/AButton.vue
@@ -1,11 +1,31 @@
 <template>
+  <v-btn
+      @click="emits('click')"
+      :variant="props.variant"
+  >
+    <div class="d-flex justify-center align-center">
+      <h4>
+        {{ props.placeholder }}
+      </h4>
+    </div>
 
+  </v-btn>
 </template>
 
-<script>
-export default {
-name: "AButton"
-}
+<script lang="ts" setup>
+import {defineProps, defineEmits} from "vue"
+
+const props = defineProps({
+  placeholder: {required: false, default: '', type: String},
+  width: {default: '', type: String},
+  height: {default: '50', type: String},
+  variant:{required:false , type:String}
+})
+
+const emits = defineEmits([
+  'click'
+])
+
 </script>
 
 <style scoped>
--- src/components/AInput.vue
+++ src/components/AInput.vue
@@ -1,13 +1,45 @@
 <template>
-
+<div>
+  <input
+  v-model="value"
+  :type="props.type"
+  class=" py-2 px-4 rounded-lg my-2"
+  />
+</div>
 </template>
 
-<script>
-export default {
-name: "AInput"
-}
+<script setup lang="ts">
+
+import {computed , defineProps ,defineEmits} from "vue";
+
+const props = defineProps({
+  modelValue:{required:true , type:[Object , String , Number]} ,
+  type:{required:true , type:String} ,
+  height:{required:false , type:String}
+
+})
+
+const eimts = defineEmits([
+    'update:modelValue'
+])
+
+const value = computed({
+get:()=>{
+  return props.modelValue
+} ,
+  set:(newValue)=>{
+  eimts('update:modelValue' , newValue)
+  }
+})
+
 </script>
 
 <style scoped>
-
+input{
+  border: 1px solid #fff;
+  color: #ffffff;
+}
+input:focus{
+  border-color: blue;
+}
 </style>
