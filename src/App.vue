030523e7c660530422d5da3722011fc87e9aa16c
Amir <amirmaleki21physics@gmail.com>
2022-10-24 16:18:46 +0330

    product list has been completed

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,18 @@
   <v-app>
     <v-main>
       <v-app-bar>
-        <div class="w-100 d-flex justify-center">
-          <h4>Hello!</h4>
+        <div class="w-100 d-flex justify-center align-center">
+          <h3>Inventory App using Vue.js </h3>
+          <span class="border rounded-circle pa-2 ml-2">{{ productsList.length }}</span>
         </div>
       </v-app-bar>
       <v-row class="pa-8">
         <v-col cols="6">
-          <add-category v-model="newCategory" @add-category="addToCategoryList"/>
-          <add-product  :categories-list="categoryList"/>
+          <add-category v-model="newCategory" @add-category="addCategoryHandler"/>
+          <add-product v-model="newProduct" :categories-list="categoryList" @add-product="addProductHandler"/>
+        </v-col>
+        <v-col cols="6">
+          <product-list :product-list="productsList"/>
         </v-col>
       </v-row>
     </v-main>
@@ -18,23 +22,27 @@
 
 <script setup lang="ts">
 import AddCategory from "@/views/AddCategory.vue";
-import {ref} from "vue";
+import {provide, ref} from "vue";
 import {addToLocalStorage, getFromLocalStorage} from "@/localStorge";
-import {Category} from "@/types";
+import {Category, Product} from "@/types";
 import AddProduct from "@/views/AddProduct.vue";
+import ProductList from "@/views/ProductList/ProductList.vue";
 
-/// this method return array of objects !
+// category section
+/// this method return array of objects
 const getCategories = getFromLocalStorage('category') ?? []
-
 const categoryList = ref<Category[]>(getCategories)
-
 const newCategory = ref({
   title: '',
   description: ''
 })
+//product section
+const getProducts = getFromLocalStorage('product') ?? []
+const productsList = ref<Product[]>(getProducts)
+const newProduct = ref({categoryTitle: 'select a category', quantity: 0, title: ""})
 
 
-function addToCategoryList() {
+function addCategoryHandler() {
   categoryList.value?.push({
     title: newCategory.value.title,
     description: newCategory.value.description,
@@ -44,5 +52,21 @@ function addToCategoryList() {
   newCategory.value.title = newCategory.value.description = ''
 }
 
+
+function addProductHandler() {
+  productsList.value?.push({
+    title: newProduct.value.title,
+    quantity: newProduct.value.quantity,
+    categoryTitle: newProduct.value.categoryTitle,
+    createDate: Date.now()
+  })
+  addToLocalStorage('product', productsList.value)
+  // reset inputs
+  newProduct.value.title = newProduct.value.categoryTitle = ''
+}
+
+
+// Prop Drilling
+provide('categoriesList', categoryList.value)
 
 </script>
--- src/views/ProductList/ProductList.vue
+++ src/views/ProductList/ProductList.vue
@@ -1,25 +1,30 @@
 <template>
-  <v-row justify="center">
+  <v-row>
     <v-col cols="12">
-      <h3 class="mb-2 font-weight-bold">Filter</h3>
-      <v-divider class="bg-white"/>
-      <section class="">
-        <div class="d-flex justify-space-between align-center pa-2">
-        <h3 class="text-info font-weight-bold">
-          Search
-        </h3>
-          <a-input  type="text"/>
+      <filter-products
+          v-model:selected-category="selectedCategory"
+          v-model:keyword-search="keyWordSearch"
+          v-model:selected-sorting-keyword="sortingKeyWord"
+      />
+      <section class="mt-3">
+        <h3>Product list </h3>
+        <v-divider class="bg-white"/>
+        <div v-if="copiedProductsList.length > 0">
+          <div
+              v-for="(product , index) in copiedProductsList"
+              :key="index"
+              class="d-flex justify-space-between  align-center pa-4"
+          >
+            <h3>{{ product.title }}</h3>
+            <div class="d-flex align-center">
+              <span>{{ convertingDate(product.createDate) }}</span>
+              <span class="pa-2 border rounded-xl mx-2">{{ product.categoryTitle }}</span>
+              <span class="rounded-circle bg-primary pa-2 mx-2">{{ product.quantity }}</span>
+              <a-button placeholder="delete" class="ml-2"/>
+            </div>
+          </div>
         </div>
-        <div class="d-flex justify-space-between align-center pa-2">
-          <h3 class="text-info font-weight-bold">
-            Sort
-          </h3>
-        </div>
-        <div class="d-flex justify-space-between align-center pa-2">
-          <h3 class="text-info font-weight-bold">
-            category
-          </h3>
-        </div>
+        <h3 v-else class="text-info text-center mt-4">There are no product.</h3>
       </section>
     </v-col>
   </v-row>
@@ -27,7 +32,58 @@
 
 <script setup lang="ts">
 
-import AInput from "@/components/AInput.vue";
+import FilterProducts from "@/views/ProductList/components/FilterProducts.vue";
+import AButton from "@/components/AButton.vue";
+import {defineProps, PropType, ref, watch} from 'vue'
+import {Product} from "@/types";
+import {convertingDate} from "@/utilities/convertDate";
+
+
+const keyWordSearch = ref('')
+const selectedCategory = ref('All')
+const sortingKeyWord = ref('select...')
+
+
+const props = defineProps({
+  productList: {required: true, type: Object as PropType<Product[]>}
+})
+const copiedProductsList = ref(props.productList)
+
+
+// when keyWordSearch changed this method call
+watch(keyWordSearch, () => {
+  const copyArray = [...props.productList]
+  if (keyWordSearch.value !== '') {
+    copiedProductsList.value = copyArray.filter((product) => {
+      return product.title.includes(keyWordSearch.value)
+    })
+  } else {
+    copiedProductsList.value = props.productList
+  }
+})
+
+// selectedCategory watcher
+watch(selectedCategory, () => {
+  const copyArray = [...props.productList]
+  if (selectedCategory.value !== 'All') {
+    copiedProductsList.value = copyArray.filter((product) => product.categoryTitle == selectedCategory.value)
+  } else {
+    copiedProductsList.value = props.productList
+
+  }
+})
+
+
+// sorting watcher
+watch(sortingKeyWord, () => {
+  if (sortingKeyWord.value === 'latest') {
+    return copiedProductsList.value.sort((a, b) => a.createDate - b.createDate
+    )
+  } else if (sortingKeyWord.value === 'earliest') {
+    return copiedProductsList.value.sort((a, b) => b.createDate - a.createDate)
+  }
+})
+
 </script>
 
 <style scoped>
